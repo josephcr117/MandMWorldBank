ac7c5571d21377374a68911da2f342af2c797d00
Josep <Josep@NELA>
2025-03-15 10:43:00 -0600

    Add project files.

---
+++ MandMWorldBank/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MandMWorldBank.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
